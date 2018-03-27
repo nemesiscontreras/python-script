48b2d1ab1913fd24a0725a390032aba1c22bd075
Nemesis Contreras <nemesisc96@gmail.com>
2018-03-27 10:27:44 -0700

    Create test_2.py

---
+++ test_2.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python3
+
+#Line 1 is the 'shebang' which tells bash to look for python 3 in /usr/bin/env python3
+
+#this is a module in python providing access to variables from the python interpreter 
+# We need to import in order to use it to print the current version of python running with sys.version
+
+import sys
+
+print("this is the current python version:\n")
+print(sys.version)
